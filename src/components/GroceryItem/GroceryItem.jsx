48b165fbf55a9737f1ed47dfc2520823a253abee
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-13 16:11:16 -0700

    refactor component GroceryItem for isEditing ?:

--- src/components/GroceryItem/GroceryItem.jsx
+++ src/components/GroceryItem/GroceryItem.jsx
@@ -8,21 +8,77 @@ export default function GroceryItem({ item }) {
     setIsEditing(e.target.checked);
   };
 
+  const handleUpdateItem = (id, payload) => {
+    // update item by id
+  };
+
+  const handleDeleteItem = (id) => {
+    // delete item by id
+  };
+
+  let content;
+  if (!isEditing) {
+    content = (
+      <>
+        <form>
+          <input
+            type="checkbox"
+            name={item.item}
+            value={item.item}
+            onChange={handleChange}
+          />
+          <label htmlFor={item.item}>{item.item}</label>
+        </form>
+        <div>
+          <button title="Update Item" onClick={() => setIsEditing(true)}>
+            ✏️
+          </button>
+          <button title="Delete Item" onClick={handleDeleteItem}>
+            ❌
+          </button>
+        </div>
+      </>
+    );
+  } else {
+    content = (
+      <>
+        <form>
+          <label htmlFor={item.item}>Update:</label>
+          <input
+            type="text"
+            name={item.item}
+            value={item.item}
+            onChange={handleUpdateItem}
+          />
+        </form>
+        <div>
+          <button title="Save Item" onClick={() => setIsEditing(false)}>
+            💾
+          </button>
+          <button title="Delete Item" onClick={handleDeleteItem}>
+            ❌
+          </button>
+        </div>
+      </>
+    );
+  }
+
   return (
-    <>
-      <form>
-        <input
-          type="checkbox"
-          name={item.item}
-          value={item.item}
-          onChange={handleChange}
-        />
-        <label htmlFor={item.item}>{item.item}</label>
-      </form>
-      <div>
-        <button title="Update Item">✏️</button>
-        <button title="Delete Item">❌</button>
-      </div>
-    </>
+    // <>
+    //   <form>
+    //     <input
+    //       type="checkbox"
+    //       name={item.item}
+    //       value={item.item}
+    //       onChange={handleChange}
+    //     />
+    //     <label htmlFor={item.item}>{item.item}</label>
+    //   </form>
+    //   <div>
+    //     <button title="Update Item">✏️</button>
+    //     <button title="Delete Item">❌</button>
+    //   </div>
+    // </>
+    content
   );
 }
