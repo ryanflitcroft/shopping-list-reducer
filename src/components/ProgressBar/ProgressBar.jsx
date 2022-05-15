2d1eb573e0d1b9ce29499a64bbff8a119762c59c
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-15 14:55:24 -0700

    component test written for ProgressBar.

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -8,11 +8,16 @@ export default function ProgressBar() {
   const progress = getAverageComplete() || 0;
 
   return (
-    <div className={Styles.container} aria-label="container for progress bar">
+    <div
+      className={Styles.container}
+      aria-label="container for progress bar"
+      data-testid="progress-container"
+    >
       <div
         className={Styles.progress}
         style={{ width: `${progress}%` }}
         aria-label="progress bar"
+        data-testid="progress-bar"
       >
         {progress}%
       </div>
