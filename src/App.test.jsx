ef4bd5508a09260b79e565e73fe709185c0d6991
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-15 15:25:35 -0700

    component GroceryList behavior test for update item text.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,13 +3,12 @@ import App from './App';
 import GroceryProvider from './context/GroceryProvider';
 
 describe('renders component App', () => {
-  render(
-    <GroceryProvider>
-      <App />
-    </GroceryProvider>
-  );
-
   it('should render layout elements header, main, footer', () => {
+    render(
+      <GroceryProvider>
+        <App />
+      </GroceryProvider>
+    );
     const header = screen.getByRole('banner');
     const main = screen.getByRole('main');
     const footer = screen.getByRole('contentinfo');
