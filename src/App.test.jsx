13c7e904597216fb96a5cc8befe086c945f05649
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-15 20:24:51 -0700

    behavior test for addItem button and text input.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 import GroceryProvider from './context/GroceryProvider';
 
@@ -12,5 +13,23 @@ describe('renders component App', () => {
     const header = screen.getByRole('banner');
     const main = screen.getByRole('main');
     const footer = screen.getByRole('contentinfo');
+  });
+
+  it('should render a new listItem with input textContent onClick of addButton', () => {
+    render(
+      <GroceryProvider>
+        <App />
+      </GroceryProvider>
+    );
+    const input = screen.getByRole('textbox');
+    const addButton = screen.getByTitle(/add item/i);
+    const listItems = screen.getAllByRole('listitem');
+
+    userEvent.type(input, 'test item');
+    userEvent.click(addButton);
+    expect(screen.getByText('test item').nodeName).toBe('LABEL');
+    expect(screen.getAllByRole('listitem').length).toEqual(
+      listItems.length + 1
+    );
   });
 });
